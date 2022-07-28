a154baad69b0480dfc17e7b977222665627fce84
swebdesigne <swebdesigne@gmail.com>
2022-07-28 14:46:18 +0300

    Состояние нити

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -1,9 +1,9 @@
 package ru.job4j.concurrent;
 
 public class ThreadState {
-    private static void checkThread(Thread th) {
-        while (th.getState() != Thread.State.TERMINATED) {
-            System.out.printf("Thread %s has status %s\n", th.getName(), th.getState());
+    private static void checkThread(Thread th1, Thread th2) {
+        while (th1.getState() != Thread.State.TERMINATED || th2.getState() != Thread.State.TERMINATED) {
+            System.out.printf("Thread %s has status %s\n", th1.getName(), th1.getState());
         }
     }
 
@@ -16,8 +16,7 @@ public class ThreadState {
                 () -> System.out.println(Thread.currentThread().getName())
         );
         second.start();
-        checkThread(first);
-        checkThread(second);
+        checkThread(first, second);
         System.out.printf("Thread %s has status %s\n"
                         + "Thread %s has status %s\n",
                 first.getName(), first.getState(),
