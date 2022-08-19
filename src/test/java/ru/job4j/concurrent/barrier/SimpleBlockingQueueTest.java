7b6f749c04d0f65783d3bd703f3bf268ad0cd6b9
Igor <swebdesigne@gmail.com>
2022-08-19 14:46:53 +0300

    Реализовать шаблон Producer Consumer

---
+++ src/test/java/ru/job4j/concurrent/barrier/SimpleBlockingQueueTest.java
@@ -0,0 +1,57 @@
+package ru.job4j.concurrent.barrier;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.stream.IntStream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class SimpleBlockingQueueTest<T> {
+    private static final int LIMIT = 7;
+
+    @Test
+    public void whenQueueNotEmpty() throws InterruptedException {
+        SimpleBlockingQueue queue = new SimpleBlockingQueue(LIMIT);
+        Queue<T> result = new LinkedList<>();
+
+        Thread thread1 = new Thread(
+                () -> {
+                    IntStream.range(0, LIMIT)
+                        .forEach(index -> queue.offer(index));
+                }
+        );
+        Thread thread2 = new Thread(
+                () -> {
+                    IntStream.range(0, LIMIT)
+                            .forEach(index -> result.add((T) queue.pool()));
+                }
+        );
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread1.join();
+        assertThat(result).isEqualTo(new LinkedList<>(List.of(0, 1, 2, 3, 4, 5, 6)));
+    }
+
+    @Test
+    public void whenQueueIsEmpty() throws InterruptedException {
+        SimpleBlockingQueue queue = new SimpleBlockingQueue(LIMIT);
+        Queue<T> consumer = new LinkedList<>();
+
+        Thread thread1 = new Thread(
+                () -> { System.out.println("Nothing was add"); }
+        );
+        Thread thread2 = new Thread(
+                () -> { consumer.add((T) queue.pool()); }
+        );
+        thread1.start();
+        thread2.start();
+        thread1.join();
+        thread1.join();
+        assertThat(consumer).isEmpty();
+    }
+
+}
