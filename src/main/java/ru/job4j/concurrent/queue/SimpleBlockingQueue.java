1968a7e90858a04be304f417dc7a23d633ca13d8
Igor <swebdesigne@gmail.com>
2022-08-22 09:33:45 +0300

    Реализовать шаблон Producer Consumer

---
+++ src/main/java/ru/job4j/concurrent/queue/SimpleBlockingQueue.java
@@ -0,0 +1,40 @@
+package ru.job4j.concurrent.queue;
+
+import net.jcip.annotations.GuardedBy;
+import net.jcip.annotations.ThreadSafe;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+@ThreadSafe
+public class SimpleBlockingQueue<T> {
+    @GuardedBy("this")
+    private Queue<T> queue = new LinkedList<>();
+    private final Object monitor = this;
+    private final int limit;
+
+    public SimpleBlockingQueue(int limit) {
+        this.limit = limit;
+    }
+
+    public void offer(T value) throws InterruptedException {
+        synchronized (monitor) {
+            if (queue.size() == limit) {
+                monitor.wait();
+            }
+            queue.offer(value);
+            monitor.notifyAll();
+        }
+    }
+
+    public T pool() throws InterruptedException {
+        synchronized (monitor) {
+            while (queue.isEmpty()) {
+                monitor.wait();
+            }
+            T poll = queue.poll();
+            monitor.notifyAll();
+            return poll;
+        }
+    }
+}
