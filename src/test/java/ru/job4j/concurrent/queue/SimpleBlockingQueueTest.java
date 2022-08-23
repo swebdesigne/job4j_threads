c276bf20d7b5cf9158dd82dc4f6abef14d617cf5
Igor <swebdesigne@gmail.com>
2022-08-23 10:27:28 +0300

    Junit тест для блокирующей очереди

--- src/test/java/ru/job4j/concurrent/queue/SimpleBlockingQueueTest.java
+++ src/test/java/ru/job4j/concurrent/queue/SimpleBlockingQueueTest.java
@@ -104,4 +104,74 @@ class SimpleBlockingQueueTest<T> {
         consumer.join();
         assertThat(buffer).isEqualTo(Arrays.asList(0, 1, 2, 3, 4, 5, 6));
     }
+
+    @Test
+    public void whenAddToQueueReverseValueAndGetIt() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(LIMIT);
+        Thread producer = new Thread(
+                () -> {
+                    for (int index = queue.getLimit(); index > 0; index--) {
+                        try {
+                            queue.offer(index);
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        producer.start();
+        Thread consumer = new Thread(
+                () -> {
+                    while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            buffer.add(queue.pool());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertThat(buffer).isEqualTo(Arrays.asList(7, 6, 5, 4, 3, 2, 1));
+    }
+
+    @Test
+    public void whenAddMoreThanQueueContainsAndGetIt() throws InterruptedException {
+        final CopyOnWriteArrayList<Integer> buffer = new CopyOnWriteArrayList<>();
+        final SimpleBlockingQueue<Integer> queue = new SimpleBlockingQueue<>(LIMIT);
+        Thread producer = new Thread(
+                () -> {
+                    for (int index = 0; index < LIMIT + 2; index++) {
+                        try {
+                            queue.offer(index);
+                        } catch (InterruptedException e) {
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        producer.start();
+        Thread consumer = new Thread(
+                () -> {
+                    while (!queue.isEmpty() || !Thread.currentThread().isInterrupted()) {
+                        try {
+                            buffer.add(queue.pool());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                        }
+                    }
+                }
+        );
+        consumer.start();
+        producer.join();
+        consumer.interrupt();
+        consumer.join();
+        assertThat(buffer).isEqualTo(Arrays.asList(0, 1, 2, 3, 4, 5, 6, 7, 8));
+    }
 }
