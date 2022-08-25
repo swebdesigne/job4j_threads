c83edca42d59fbe1b3b89b3e9fccb216a3d8bb45
Igor <swebdesigne@gmail.com>
2022-08-25 11:09:06 +0300

    Неблокирующий кеш

--- src/main/java/ru/job4j/concurrent/cache/OptimisticException.java
+++
@@ -1,7 +0,0 @@
-package ru.job4j.concurrent.cache;
-
-public class OptimisticException extends RuntimeException {
-    public OptimisticException(String message) {
-        super(message);
-    }
-}
